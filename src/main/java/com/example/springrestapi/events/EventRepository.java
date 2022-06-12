20c40bd2ec5558f7a4d82ae67ac71a3181aa0a12
Seungmin <globalsh1@gmail.com>
2022-06-12 22:16:50 +0900

    POST /api/events returns 201

---
+++ src/main/java/com/example/springrestapi/events/EventRepository.java
@@ -0,0 +1,7 @@
+package com.example.springrestapi.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+
+}
