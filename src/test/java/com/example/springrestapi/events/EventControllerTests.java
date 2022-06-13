02e6d2c8e9a17c5f4e004879303942d8ffec979c
Seungmin <globalsh1@gmail.com>
2022-06-13 20:11:31 +0900

    fail on unknown properties

--- src/test/java/com/example/springrestapi/events/EventControllerTests.java
+++ src/test/java/com/example/springrestapi/events/EventControllerTests.java
@@ -34,6 +34,38 @@ public class EventControllerTests {
     @Test
     public void createEvent() throws Exception {
 
+        EventDto event = EventDto.builder()
+                                 .name("Spring")
+                                 .description("REST API Development with Spring")
+                                 .beginEnrollmentDateTime(LocalDateTime.of(2022, 06, 12, 22, 00))
+                                 .closeEnrollmentDateTime(LocalDateTime.of(2022, 06, 13, 22, 00))
+                                 .beginEventDateTime(LocalDateTime.of(2022, 06, 14, 22, 00))
+                                 .endEventDateTime(LocalDateTime.of(2022, 06, 15, 22, 00))
+                                 .basePrice(100)
+                                 .maxPrice(200)
+                                 .limitOfEnrollment(100)
+                                 .location("강남역 D2 스타텁 팩토리")
+                                 .build();
+
+//        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
+        mockMvc.perform(post("/api/events/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaTypes.HAL_JSON)
+                                .content(objectMapper.writeValueAsString(event)))
+               .andDo(print())
+               .andExpect(status().isCreated())
+               .andExpect(jsonPath("id").exists())
+               .andExpect(header().exists(HttpHeaders.LOCATION))
+               .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+               .andExpect(jsonPath("id").value(Matchers.not(100)))
+               .andExpect(jsonPath("free").value(Matchers.not(true)))
+               .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+    }
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
+
         Event event = Event.builder()
                            .id(100)
                            .name("Spring")
@@ -54,16 +86,10 @@ public class EventControllerTests {
 //        Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events/")
-                       .contentType(MediaType.APPLICATION_JSON)
-                       .accept(MediaTypes.HAL_JSON)
-                       .content(objectMapper.writeValueAsString(event)))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaTypes.HAL_JSON)
+                                .content(objectMapper.writeValueAsString(event)))
                .andDo(print())
-               .andExpect(status().isCreated())
-               .andExpect(jsonPath("id").exists())
-               .andExpect(header().exists(HttpHeaders.LOCATION))
-               .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-               .andExpect(jsonPath("id").value(Matchers.not(100)))
-               .andExpect(jsonPath("free").value(Matchers.not(true)))
-               .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+               .andExpect(status().isBadRequest());
     }
 }
