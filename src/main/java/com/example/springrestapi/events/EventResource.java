f388d293bdb77c4130694e88e3eb2995f2e2d2db
Seungmin <globalsh1@gmail.com>
2022-06-14 17:35:07 +0900

    add hateoas

---
+++ src/main/java/com/example/springrestapi/events/EventResource.java
@@ -0,0 +1,12 @@
+package com.example.springrestapi.events;
+
+import org.springframework.hateoas.EntityModel;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+public class EventResource extends EntityModel<Event> {
+    public EventResource(Event content) {
+        super(content);
+        add(linkTo(EventController.class).slash(content.getId()).withSelfRel());
+    }
+}
